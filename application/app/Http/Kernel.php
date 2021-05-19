239a8fecf398be9c73c698ff26746342d213dd61
Leonardo Lazarini <llazarini2@gmail.com>
2021-05-19 20:55:29 -0300

    Development;

--- application/app/Http/Kernel.php
+++ application/app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\Cors;
 use App\Http\Middleware\RequestUniqueId;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
@@ -15,6 +16,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
+        Cors::class,
         \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
         //\Fruitcake\Cors\HandleCors::class,
