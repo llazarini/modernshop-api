b045ed934360baef2e78137fddaff446583489ab
Leonardo Lazarini <llazarini2@gmail.com>
2021-05-24 14:48:30 -0300

    Development;

--- application/app/Http/Middleware/Cors.php
+++ application/app/Http/Middleware/Cors.php
@@ -17,7 +17,7 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', env('APP_STORE_URL'))
+            ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', "PUT, POST, DELETE, GET, OPTIONS")
             ->header('Access-Control-Allow-Headers', "Accept, Authorization, Content-Type");
     }
