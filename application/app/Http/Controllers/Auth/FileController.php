e51924cb7f322b17c4e49684d73e40deba3fc486
Leonardo Lazarini <llazarini2@gmail.com>
2021-06-02 22:32:11 -0300

    Image size improved;

--- application/app/Http/Controllers/Auth/FileController.php
+++ application/app/Http/Controllers/Auth/FileController.php
@@ -25,7 +25,7 @@ class FileController extends Controller
         $file->type = $request->get('type');
         $file->type_id = $request->get('type_id');
         $typeUrl = Str::slug($file->type);
-        if (env('APP_ENV') == 'production') {
+        if (!(env('APP_ENV') == 'local')) {
             Image::configure(array('driver' => 'imagick'));
         }
         $image = Image::make($requestFile);
