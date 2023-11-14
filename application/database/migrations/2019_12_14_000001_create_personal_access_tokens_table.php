a2c333ffe1885c0146e2bf99efc835797dbb4420
Leonardo Lazarini <llazarini2@gmail.com>
2023-11-14 18:25:00 +0000

    Updates

--- application/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ application/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -14,11 +14,13 @@ class CreatePersonalAccessTokensTable extends Migration
     public function up()
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->id();
             $table->morphs('tokenable');
             $table->string('name');
             $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
+            // Add here
+            $table->timestamp('expires_at')->nullable();
             $table->timestamp('last_used_at')->nullable();
             $table->timestamps();
         });
